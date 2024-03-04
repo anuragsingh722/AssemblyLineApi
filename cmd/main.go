c5b9853ce05f08751be01a1214891f69a69f46a4
Anurag Singh <cultofdanurag@gmail.com>
2024-03-04 11:16:50 +0530

    initial api

---
+++ cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+)
+
+func Init() *gorm.DB {
+	dbURL := "postgres://pg:pass@localhost:5432/crud"
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(
+		Furniture{},
+		Panel{})
+
+	return db
+}
+
+type Furniture struct {
+	gorm.Model
+	Sku    string `json:"sku"`
+	Panels []Panel
+}
+
+type Panel struct {
+	gorm.Model
+	FurnitureID uint
+	Furniture   Furniture
+}
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
